48f92dd4e5e18d717e05ed9710336ed906525160
Daniil Shadrin <rabbit72rus@gmail.com>
2018-12-10 20:54:12 +0300

    add new features

--- db.py
+++ db.py
@@ -13,52 +13,78 @@ Base = declarative_base()
 class Location(Base):
     __tablename__ = "location"
     id = Column(Integer, primary_key=True)
-    name = Column(String(32))
+    title = Column(String(32))
     latitude = Column(Float)
     longitude = Column(Float)
     image = Column(String(64), nullable=True)
     description = Column(String(256), nullable=True)
     owner = Column(ForeignKey("user.id"))
 
-    def __init__(self, name, coordinates, image=None, description=None, owner=None):
-        self.name = name
-        self.latitude, self.longitude = coordinates
+    def __init__(
+            self,
+            title=None,
+            latitude=None,
+            longitude=None,
+            image=None,
+            description=None,
+            owner=None
+    ):
+        self.title = title
+        self.latitude = latitude
+        self.longitude = longitude
         self.image = image
         self.description = description
         self.owner = owner
 
     def __repr__(self):
-        pass  # TODO
+        return f"Location <{self.latitude}, {self.longitude}>"
 
 
 class User(Base):
     __tablename__ = "user"
     id = Column(Integer, primary_key=True)
-    chat_id = Column(Integer, nullable=False)
+    stage = Column(Integer, default=0)
 
     def __init__(self, chat_id):
-        self.chat_id = chat_id
+        self.id = chat_id
 
-    def add_location(self, name, coordinates, image=None, description=None):
-        location = Location(name, coordinates, image, description, owner=self.id)
+    def add_location(self, location: Location):
+        location.owner = self.id
         session.add(location)
+        session.commit()
+
+    def change_stage(self, stage: int):
+        self.stage = stage
+        session.commit()
+
+    def delete_all_locations(self):
+        session.query(Location).filter(Location.owner == self.id).delete()
         session.commit()
 
     @property
     def locations(self):
-        r = session.query(Location).filter(Location.owner == self.id)
+        r = session.query(Location).filter(Location.owner == self.id).order_by("-id").limit(10)
         return list(r)
 
     def __repr__(self):
-        pass  # TODO
+        return f"User <id: {self.id}>"
+
+
+def get_user(message):
+    chat_id = message.chat.id
+    current_user = session.query(User).get(chat_id)
+    if not current_user:
+        current_user = User(chat_id)
+        session.add(current_user)
+        session.commit()
+    return current_user
 
 
 Base.metadata.create_all(engine)
-
+Session = sessionmaker(bind=engine)
+session = Session()
 
 if __name__ == "__main__":
-    Session = sessionmaker(bind=engine)
-    session = Session()
     # user = User(34534543)
     # session.add(user)
     # session.commit()
--- location_bot.py
+++ location_bot.py
@@ -1,23 +1,144 @@
 import os
 import telebot
+from db import get_user, Location
 
 
 BOT_TOKEN = os.getenv("BOT_TOKEN") or open("../BOT_TOKEN.txt").read()
-
+START, TITLE, LOCATION, ADDITIONAL, IMAGE, DESCRIPTION, SAVE = range(7)
+TEMP = dict()
+CONFIRMATION = "y", "yes", "yea", "ok", "д", "да", "ок"
+CANCELLATION = "n", "no", "not", "н", "не", "нет"
 bot = telebot.TeleBot(BOT_TOKEN)
 
 
-@bot.message_handler(commands=["start"])
+@bot.message_handler(commands=["start", "help"])
 def handle_start(message):
-    bot.send_message(message.chat.id, "Hi, I can save locations for you")
+    get_user(message)
+    bot.send_message(message.chat.id, "Welcome, I can save locations for you")
 
 
-@bot.message_handler()
+@bot.message_handler(
+    commands=["add"],
+    func=lambda message: get_user(message).stage == START
+)
+def handle_add(message):
+    TEMP[message.chat.id] = Location()
+    bot.send_message(message.chat.id, "Let's save your location\nSend me title")
+    user = get_user(message)
+    user.change_stage(TITLE)
+
+
+@bot.message_handler(func=lambda message: get_user(message).stage == TITLE)
+def handle_title(message):
+    TEMP[message.chat.id].title = message.text.title()
+    bot.send_message(message.chat.id, "Send location")
+    user = get_user(message)
+    user.change_stage(LOCATION)
+
+
+@bot.message_handler(
+    func=lambda message: get_user(message).stage == LOCATION,
+    content_types=["location"]
+)
+def handle_location(message):
+    TEMP[message.chat.id].latitude = message.location.latitude
+    TEMP[message.chat.id].longitude = message.location.longitude
+    bot.send_message(message.chat.id, "Do you want to add additional information?")
+    user = get_user(message)
+    user.change_stage(ADDITIONAL)
+
+
+@bot.message_handler(func=lambda message: get_user(message).stage == ADDITIONAL)
+def handle_additional(message):
+    user = get_user(message)
+    lower_message = message.text.lower()
+    if any(word in lower_message for word in CONFIRMATION):
+        user.change_stage(IMAGE)
+        bot.send_message(message.chat.id, "Send an image of location")
+    elif any(word in lower_message for word in CANCELLATION):
+        user.change_stage(SAVE)
+        send_temp_location(message)
+        bot.send_message(message.chat.id, "Do you want to save the location?")
+
+
+@bot.message_handler(
+    func=lambda message: get_user(message).stage == IMAGE,
+    content_types=["photo"]
+)
+def handle_image(message):
+    user = get_user(message)
+    TEMP[message.chat.id].image = message.photo[2].file_id
+    bot.send_message(message.chat.id, "Send location description")
+    user.change_stage(DESCRIPTION)
+
+
+@bot.message_handler(func=lambda message: get_user(message).stage == DESCRIPTION)
+def handle_description(message):
+    user = get_user(message)
+    TEMP[message.chat.id].description = message.text
+    user.change_stage(SAVE)
+
+    send_temp_location(message)
+    bot.send_message(message.chat.id, "Do you want to save the location?")
+
+
+@bot.message_handler(func=lambda message: get_user(message).stage == SAVE)
+def handle_save(message):
+    chat_id = message.chat.id
+    user = get_user(message)
+    lower_message = message.text.lower()
+    if any(word in lower_message for word in CONFIRMATION):
+        bot.send_message(chat_id, "The location was saved")
+        user.add_location(TEMP[chat_id])
+    elif any(word in lower_message for word in CANCELLATION):
+        bot.send_message(chat_id, "The location wasn't saved")
+    TEMP[chat_id] = None
+    user.change_stage(START)
+
+
+@bot.message_handler(commands=["list"])
+def handle_list(message):
+    user = get_user(message)
+    if user.locations:
+        for location in user.locations:
+            send_location(message.chat.id, location)
+    else:
+        bot.send_message(message.chat.id, "You don't have locations")
+
+
+@bot.message_handler(commands=["reset"])
+def handle_reset(message):
+    user = get_user(message)
+    user.delete_all_locations()
+    bot.send_message(message.chat.id, "Your locations were deleted")
+
+
+@bot.message_handler(content_types=["text"])
 def handle_not_realized_message(message):
     bot.send_message(
         message.chat.id,
         "I don't understand you, choose one of the my commands"
     )
+
+
+def send_location(chat_id, location: Location):
+    bot.send_message(chat_id, f"Title: {location.title}")
+    bot.send_location(chat_id, location.latitude, location.longitude)
+    bot.send_photo(chat_id, location.image, caption=location.description)
+
+
+def send_temp_location(message):
+    chat_id = message.chat.id
+    title = TEMP[message.chat.id].title
+    latitude = TEMP[chat_id].latitude
+    longitude = TEMP[chat_id].longitude
+    image = TEMP[chat_id].image
+    description = TEMP[chat_id].description
+
+    bot.send_message(chat_id, f"Title: {title}")
+    bot.send_location(chat_id, latitude, longitude)
+    if image and description:
+        bot.send_photo(chat_id, image, caption=description)
 
 
 if __name__ == "__main__":
