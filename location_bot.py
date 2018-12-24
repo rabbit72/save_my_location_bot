42a3fecdfc5b9eda698136e79bc9461c1d8ddb99
Daniil Shadrin <rabbit72rus@gmail.com>
2018-12-24 16:15:26 +0300

    add feature - find locations ner user

--- location_bot.py
+++ location_bot.py
@@ -1,9 +1,12 @@
 import os
+from typing import Tuple
+import requests
 import telebot
 from db import get_user, Location
 
-
+Coordinates = Tuple[float, float]
 BOT_TOKEN = os.getenv("BOT_TOKEN") or open("../BOT_TOKEN.txt").read()
+MAP_TOKEN = os.getenv("MAP_TOKEN") or open("../MAP_TOKEN.txt").read()
 START, TITLE, LOCATION, ADDITIONAL, IMAGE, DESCRIPTION, SAVE = range(7)
 TEMP = dict()
 CONFIRMATION = "y", "yes", "yea", "ok", "д", "да", "ок"
@@ -18,8 +21,7 @@ def handle_start(message):
 
 
 @bot.message_handler(
-    commands=["add"],
-    func=lambda message: get_user(message).stage == START
+    commands=["add"], func=lambda message: get_user(message).stage == START
 )
 def handle_add(message):
     TEMP[message.chat.id] = Location()
@@ -37,8 +39,7 @@ def handle_title(message):
 
 
 @bot.message_handler(
-    func=lambda message: get_user(message).stage == LOCATION,
-    content_types=["location"]
+    func=lambda message: get_user(message).stage == LOCATION, content_types=["location"]
 )
 def handle_location(message):
     TEMP[message.chat.id].latitude = message.location.latitude
@@ -62,8 +63,7 @@ def handle_additional(message):
 
 
 @bot.message_handler(
-    func=lambda message: get_user(message).stage == IMAGE,
-    content_types=["photo"]
+    func=lambda message: get_user(message).stage == IMAGE, content_types=["photo"]
 )
 def handle_image(message):
     user = get_user(message)
@@ -99,8 +99,9 @@ def handle_save(message):
 @bot.message_handler(commands=["list"])
 def handle_list(message):
     user = get_user(message)
-    if user.locations:
-        for location in user.locations:
+    locations = user.locations[:10]
+    if locations:
+        for location in locations:
             send_location(message.chat.id, location)
     else:
         bot.send_message(message.chat.id, "You don't have locations")
@@ -116,15 +117,32 @@ def handle_reset(message):
 @bot.message_handler(content_types=["text"])
 def handle_not_realized_message(message):
     bot.send_message(
-        message.chat.id,
-        "I don't understand you, choose one of the my commands"
+        message.chat.id, "I don't understand you, choose one of the my commands"
     )
+
+
+@bot.message_handler(content_types=["location"])
+def handle_find_locations_near_me(message):
+    current_point = (message.location.latitude, message.location.longitude)
+    user = get_user(message)
+    locations_500m_from_user = [
+        loc
+        for loc in user.locations
+        if is_locations_closer_500m(current_point, (loc.latitude, loc.longitude))
+    ]
+    if locations_500m_from_user:
+        bot.send_message(message.chat.id, "The locations were found close to you:")
+        for loc in locations_500m_from_user:
+            send_location(message.chat.id, loc)
+    else:
+        bot.send_message(message.chat.id, "The locations weren't found around you")
 
 
 def send_location(chat_id, location: Location):
     bot.send_message(chat_id, f"Title: {location.title}")
     bot.send_location(chat_id, location.latitude, location.longitude)
-    bot.send_photo(chat_id, location.image, caption=location.description)
+    if location.image and location.description:
+        bot.send_photo(chat_id, location.image, caption=location.description)
 
 
 def send_temp_location(message):
@@ -141,5 +159,25 @@ def send_temp_location(message):
         bot.send_photo(chat_id, image, caption=description)
 
 
+def is_locations_closer_500m(start_point: Coordinates, end_point: Coordinates) -> bool:
+    meters_500 = 500
+    url_api = "https://maps.googleapis.com/maps/api/distancematrix/json"
+
+    start_latitude, start_longitude = start_point
+    end_latitude, end_longitude = end_point
+    origins = f"{start_latitude},{start_longitude}"
+    destinations = f"{end_latitude},{end_longitude}"
+
+    params = {
+        "key": MAP_TOKEN,
+        "origins": origins,
+        "destinations": destinations,
+        "mode": "walking",
+    }
+    answer = requests.get(url_api, params=params).json()
+    distance = answer["rows"][0]["elements"][0]["distance"]["value"]
+    return True if distance <= meters_500 else False
+
+
 if __name__ == "__main__":
-    bot.polling()
+    bot.polling(none_stop=True)
